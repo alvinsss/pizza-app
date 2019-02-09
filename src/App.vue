6618b24603e494bd726d31c18af7ba21a1521a0e
alvin <6449694@qq.com>
2019-02-09 21:04:15 +0800

    Initial commit 搭建和菜单导航

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-header></app-header>
+    </div>
+  </div>
+</template>
+
+<script>
+// 引入header
+import Header from './components/Header';
+// 注册组件header header跟标签冲突，我们重起appHeader
+export default {
+  components:{
+     "app-header":Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import VueRouter from 'vu'
+import App from './App.vue'
+
+new Vue({
+
+  el: '#app',
+  render: h => h(App)
+
+})
