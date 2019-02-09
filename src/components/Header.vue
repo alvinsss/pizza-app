c22a6c5f017a969e6823a22ea72b5002ad4dc93c
alvin <6449694@qq.com>
2019-02-09 21:20:36 +0800

    路由

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,11 +8,15 @@
       <a href="/" class="navbar-brand">Pizz点餐系统</a>
 
       <ul class="navbar-nav">
-        <li> <a herf="#" class="nav-link">登录</a></li>
-        <li> <a herf="#" class="nav-link">菜单</a></li>
-        <li> <a herf="#" class="nav-link">管理</a></li>
-        <li> <a herf="#" class="nav-link">关于我们</a></li>
+        <li> <router-link to="/" class="nav-link"> 主页 </router-link></li>
+        <li> <router-link to="/menu" class="nav-link"> 菜单 </router-link></li>
+        <li> <router-link to="/admin" class="nav-link"> 管理 </router-link></li>
+        <li> <router-link to="/about" class="nav-link"> 关于我们 </router-link></li>
+      </ul>
 
+      <ul class="navbar-nav ml-auto">
+        <li><router-link to="/login" class="nav-link"> 登录 </router-link></li>
+        <li><router-link to="/register" class="nav-link"> 注册 </router-link></li>
       </ul>
 
     </nav>
